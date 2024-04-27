27ea969cf49a23cb254d2e285c19840de85d7433
Roland Walker <walker@pobox.com>
2024-04-27 12:27:12 -0400

    reorganize examples directory
    moving all in to "succeeding" and "failing" sections

--- script/parse_examples.sh
+++ script/parse_examples.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-known_failures="$(cat script/known_failures.txt)"
-
 tree-sitter parse -q -t \
-  examples/**/*.pgn \
-  $(for failure in $known_failures; do printf '%s\n' "!${failure}"; done)
+  examples/succeeding/**/*.pgn \
 
 #
